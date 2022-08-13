4d43c0ba1f76c5c0991ee3f24818cb87789a7239
YelzhasTaubay <elzhas.96@mail.ru>
2022-08-14 01:31:15 +0600

    Starting new project

---
+++ src/main/java/com/example/YelzhasDemoBot/YelzhasDemoBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.YelzhasDemoBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YelzhasDemoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YelzhasDemoBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/YelzhasDemoBot/YelzhasDemoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.YelzhasDemoBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YelzhasDemoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
